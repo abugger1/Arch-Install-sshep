3d7418f6eeebd2e74dc3cf3ef27bbc306872c146
abugger1 <2695670+abugger1@users.noreply.github.com>
2022-09-08 20:28:40 -0400

    update 5

--- Arch-Base-Install.sh
+++ Arch-Base-Install.sh
@@ -14,12 +14,13 @@ reflector --country 'United States' --latest 20 --protocol https --sort rate --s
 # cd pikaur/
 # makepkg -si --noconfirm
 
-
+pacman-key --init
+pacman-key --populate
 pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key FBA220DFC880C036
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
-pacman -Sy github-cli grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers-zen avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g timeshift-bin timeshift-autosnap
+pacman -Syy github-cli grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-zen-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g
 
 
 # pacman -S --noconfirm xf86-video-amdgpu
@@ -30,7 +31,6 @@ grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB #cha
 
 grub-mkconfig -o /boot/grub/grub.cfg
 
-mkinitcpio -p linux-zen
 
 systemctl enable NetworkManager
 systemctl enable bluetooth
