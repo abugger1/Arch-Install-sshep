9967be9e60f983c05dd12e6e0a4abe72504227b5
abugger1 <2695670+abugger1@users.noreply.github.com>
2022-09-09 00:02:57 -0400

    update 8

--- Arch-Base-Install-KDE.sh
+++ Arch-Base-Install-KDE.sh
@@ -40,6 +40,6 @@ sudo pacman -S xorg sddm plasma vlc akonadi-calendar-tools akonadiconsole alsa-u
 pacman -Sy $$ powerpill -Su && pikaur -Su
 
 sudo systemctl enable sddm
-# /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
-# sleep 5
-# reboot
+/bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
+sleep 5
+reboot
