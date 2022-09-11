ff9ed3c821716f7dd55b19ae89c73be72e84c0c2
abugger1 <2695670+abugger1@users.noreply.github.com>
2022-09-10 23:05:08 -0400

    updated scripts

--- Arch-Base-Install-KDE.sh
+++ Arch-Base-Install-KDE.sh
@@ -1,45 +1,49 @@
 #!/bin/bash
+#
+ sudo timedatectl set-ntp true
+ sudo hwclock --systohc
 
-sudo timedatectl set-ntp true
-sudo hwclock --systohc
+# sudo reflector --country 'United States' --latest 20 --protocol https --sort rate --save /etcpacman.d/mirrorlist
 
-sudo reflector --country 'United States' --latest 20 --protocol https --sort rate --save /etcpacman.d/mirrorlist
+ sudo firewall-cmd --add-port=1025-65535/tcp --permanent
+ sudo firewall-cmd --add-port=1025-65535/udp --permanent
+ sudo firewall-cmd --reload
 
-sudo firewall-cmd --add-port=1025-65535/tcp --permanent
-sudo firewall-cmd --add-port=1025-65535/udp --permanent
-sudo firewall-cmd --reload
+# git clone https://aur.archlinux.org/pikaur.git
+# cd pikaur/
+# makepkg -si --noconfirm
 
-git clone https://aur.archlinux.org/pikaur.git
-cd pikaur/
-makepkg -si --noconfirm
+ sudo pikaur -S powerpill zramd
 
-pikaur -S powerpill zramd
+ sudo systemctl enable --now zramd.service
 
-sudo systemctl enable --now zramd.service
-
-sudo powerpill -S ttf-fira-code zsh zsh-completions kitty-git kitty-terminfo timeshift-bin timeshift-autosnap
-
-wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-sh install.sh
-
-cd ~
-wget https://github.com/dracula/zsh/archive/master.zip
-uinzip master.zip
-cd master
-mv dracula.zsh-theme $HOME/.oh-my-zsh/themes
-
+ sudo powerpill -S ttf-fira-code zsh zsh-completions kitty-git kitty-terminfo timeshift-bin timeshift-autosnap
+#
+#
+ cd ~
+ mkdir -p $HOME/.config/zsh/lib
+ touch $HOME/.config/zsh/zsh-prompt
+ wget https://github.com/dracula/zsh/archive/master.zip
+ uinzip master.zip
+ cd master
+ mv lib/async.zsh $HOME/.config/zsh/lib
+ mv dracula.zsh-theme $HOME/.config/zsh/zsh-prompt
+ cd ~
+ rm -r {master.zip,master}
+# 
+#
 #pikaur -S --noconfirm system76-power
 #sudo systemctl enable --now system76-power
 #sudo system76-power graphics integrated
 #pikaur -S --noconfirm auto-cpufreq
 #sudo systemctl enable --now auto-cpufreq
-
-
-sudo pacman -S xorg sddm plasma vlc akonadi-calendar-tools akonadiconsole alsa-utils  ark artikulate audiocd-kio autoconf automake base bison blinken bluez-utils bomber bovo cantor cervisia dialog discord dolphin-plugins dragon elisa fakeroot filelight firedragon firefox flex foxitreader grantlee-editor gwenview hplip htop inetutils itinerary juk kate kcalc kcharselect kcolorchooser kcron kde-dev-scripts kde-dev-utils kde-gtk-config kdebugsettings kdenetwork-filesharing kdeplasma-addons kdesdk-kio kdesdk-thumbnailers kdf kdialog kfind kfloppy kgamma5 kget kgpg  khelpcenter khotkeys kio-gdrive kio-zeroconf kmix kmousetool kontrast kruler ksysguard ksystemlog kteatime ktorrent kwallet-pam kwayland-integration libreoffice-fresh lightdm-webkit-theme-litarvan linux-xanmod-edge-headers linux-zen-headers lutris modprobed-db mpv neofetch network-manager-applet okular oxygen palapeli partitionmanager pkgconf plasma-browser-integration plasma-disks plasma-firewall plasma-pa plasma-sdk plasma-systemmonitor plasma-thunderbolt plasma-vault poxml print-manager python-jedi python-pipenv qbittorrent sddm-kcm spectacle steam stow sweeper telepathy-kde-approver telepathy-kde-auth-handler telepathy-kde-call-ui telepathy-kde-contact-list telepathy-kde-contact-runner telepathy-kde-desktop-applets telepathy-kde-filetransfer-handler telepathy-kde-send-file telepathy-kde-text-ui tutanota-desktop-bin vim webtorrent-desktop xorg-bdftopcf xorg-docs xorg-font-util xorg-fonts-100dpi xorg-fonts-75dpi xorg-iceauth xorg-mkfontscale xorg-server-devel xorg-server-xnest xorg-server-xvfb xorg-sessreg xorg-smproxy xorg-x11perf xorg-xbacklight xorg-xcmsdb xorg-xcursorgen xorg-xdriinfo xorg-xev xorg-xhost xorg-xinput xorg-xkbevd xorg-xkbutils xorg-xkill xorg-xlsatoms xorg-xlsclients xorg-xpr xorg-xrefresh xorg-xvinfo xorg-xwayland xorg-xwd xorg-xwud yakuake
-
-pacman -Sy $$ powerpill -Su && pikaur -Su
-
-sudo systemctl enable sddm
-/bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
-sleep 5
-reboot
+#
+#
+ sudo pacman -S xorg sddm plasma vlc akonadi-calendar-tools alsa-utils ark artikulate audiocd-kio autoconf automake base bison blinken bluez-utils bomber bovo cantor cervisia dialog dolphin-plugins dragon elisa fakeroot filelight flex foxitreader grantlee-editor gwenview hplip htop inetutils itinerary juk kate kcalc kcharselect kcolorchooser kcron kde-dev-scripts kde-dev-utils kde-gtk-config kdebugsettings kdenetwork-filesharing kdeplasma-addons kdesdk-kio kdesdk-thumbnailers kdf kdialog kfind kfloppy kgamma5 kget kgpg  khelpcenter khotkeys kio-gdrive kio-zeroconf kmix kmousetool kontrast kruler ksysguard ksystemlog kteatime ktorrent kwallet-pam kwayland-integration libreoffice-fresh lightdm-webkit-theme-litarvan linux-xanmod-edge-headers linux-zen-headers lutris modprobed-db mpv neofetch network-manager-applet okular oxygen palapeli partitionmanager pkgconf plasma-browser-integration plasma-disks plasma-firewall plasma-pa plasma-sdk plasma-systemmonitor plasma-thunderbolt plasma-vault poxml print-manager python-jedi python-pipenv qbittorrent sddm-kcm spectacle stow sweeper telepathy-kde-approver telepathy-kde-auth-handler telepathy-kde-call-ui telepathy-kde-contact-list telepathy-kde-contact-runner telepathy-kde-desktop-applets telepathy-kde-filetransfer-handler telepathy-kde-send-file telepathy-kde-text-ui bin vim xorg-bdftopcf xorg-docs xorg-font-util xorg-fonts-100dpi xorg-fonts-75dpi xorg-iceauth xorg-mkfontscale xorg-server-devel xorg-server-xnest xorg-server-xvfb xorg-sessreg xorg-smproxy xorg-x11perf xorg-xbacklight xorg-xcmsdb xorg-xcursorgen xorg-xdriinfo xorg-xev xorg-xhost xorg-xinput xorg-xkbevd xorg-xkbutils xorg-xkill xorg-xlsatoms xorg-xlsclients xorg-xpr xorg-xrefresh xorg-xvinfo xorg-xwayland xorg-xwd xorg-xwud yakuake
+#
+ sudo pacman -Sy && sudo powerpill -Su && pikaur -Su
+#
+ sudo systemctl enable sddm
+# /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
+# sleep 5
+# reboot
--- Arch-Base-Install.sh
+++ Arch-Base-Install.sh
@@ -1,54 +1,68 @@
 #!/bin/bash
 
 # INSTALL SCRIPT FOR ARCH PROGRAMS I USE
-# Updating repos, system and mirrors
-
-reflector --country 'United States' --latest 20 --protocol https --sort rate --save /etcpacman.d/mirrorlist
-
+# Updating mirrorlist
+#
+ reflector --country 'United States' --latest 20 --protocol https --sort rate --save /etcpacman.d/mirrorlist
+#
 # Install Ucode
-
+#
 # pacman -S --noconfirm amd-ucode
 # pacman -S --noconfirm intel-ucode
-
-# git clone https://aur.archlinux.org/pikaur.git
-# cd pikaur/
-# makepkg -si --noconfirm
-
-pacman-key --init
-pacman-key --populate
-pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
-pacman-key --lsign-key FBA220DFC880C036
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-
-pacman -Syy github-cli grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-zen-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g
-
-
+#
+#
+#
+# Install pikaur
+ git clone https://aur.archlinux.org/pikaur.git
+ cd pikaur
+ makepkg -si --noconfirm
+ cd
+ rm -r pikaur
+#
+#
+#
+#
+#
+# Install chaoticAUR and mirrorlist
+ pacman -S archlinux-keyring
+ pacman-key --init
+ pacman-key --populate
+ pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+ pacman-key --lsign-key FBA220DFC880C036
+ pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+ cp pacman.conf /etc/pacman.conf
+ cp makepkg.conf /etc/makepkg.conf
+#
+#
+ pacman -Syy github-cli efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-zen-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g
+# pacman -S grub   
+#
 # pacman -S --noconfirm xf86-video-amdgpu
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
-
-
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB #change the directory to /boot/efi if you mounted the EFI partition at /boot/efi
-
-grub-mkconfig -o /boot/grub/grub.cfg
-
-
-systemctl enable NetworkManager
-systemctl enable bluetooth
-systemctl enable cups.service
-systemctl enable sshd
-systemctl enable avahi-daemon
-systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
-systemctl enable reflector.timer
-systemctl enable fstrim.timer
-systemctl enable libvirtd
-systemctl enable firewalld
-systemctl enable acpid
-
-useradd -m sshep
-echo sshep:password | chpasswd
+#
+#
+# grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB #change the directory to /boot/efi if you mounted the EFI partition at /boot/efi
+#
+# grub-mkconfig -o /boot/grub/grub.cfg
+#
+#
+ systemctl enable NetworkManager
+ systemctl enable bluetooth
+ systemctl enable cups.service
+ systemctl enable sshd
+ systemctl enable avahi-daemon
+ systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
+ systemctl enable reflector.timer
+ systemctl enable fstrim.timer
+ systemctl enable libvirtd
+ systemctl enable firewalld
+ systemctl enable acpid
+#
+ useradd -m sshep
+ echo sshep:password | chpasswd
 # usermod -aG libvirt "USER"
-
-echo "sshep ALL=(ALL) ALL" >> /etc/sudoers.d/sshep
-
-
-printf "\e[1;32mDone! Type exit, umount /mnt -R and reboot.\e[0m"
+#
+ echo "sshep ALL=(ALL) ALL" >> /etc/sudoers.d/sshep
+#
+#
+ printf "\e[1;32mDone! Type exit, umount /mnt -R and reboot.\e[0m"
