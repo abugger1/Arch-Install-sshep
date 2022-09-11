5f2b4eb21a825286ea72ba45d4309e14e5bb1c86
abugger1 <2695670+abugger1@users.noreply.github.com>
2022-09-11 14:56:06 -0400

    updated scripts.00005

--- Arch-Base-Install.sh
+++ Arch-Base-Install.sh
@@ -37,6 +37,7 @@
  pacman -Syy github-cli efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-zen-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g
 # pacman -S grub   
 #
+# pacman -S --noconfirm xf86-video-intel
 # pacman -S --noconfirm xf86-video-amdgpu
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 #
