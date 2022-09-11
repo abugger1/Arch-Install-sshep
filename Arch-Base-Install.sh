3f4ab78347a57719d33ece89eb91bc431b7d2399
abugger1 <2695670+abugger1@users.noreply.github.com>
2022-09-11 15:07:23 -0400

    updated scripts.00007

--- Arch-Base-Install.sh
+++ Arch-Base-Install.sh
@@ -24,8 +24,8 @@
  pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
  pacman-key --lsign-key FBA220DFC880C036
  pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
- cp pacman.conf /etc/pacman.conf
- cp makepkg.conf /etc/makepkg.conf
+ cp /arch-install-sshep/pacman.conf /etc/pacman.conf
+ cp /arch-install-sshep/makepkg.conf /etc/makepkg.conf
 #
 #
  pacman -Syy github-cli efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-zen-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g
