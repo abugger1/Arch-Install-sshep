ad2415d8650074fa90a239e7c0acbd4c3fa6e614
abugger1 <2695670+abugger1@users.noreply.github.com>
2022-09-12 02:06:49 -0400

    updated locale section

--- Arch-Base-Install.sh
+++ Arch-Base-Install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+ln -sf /usr/share/zoneinfo/US/Eastern /etc/localtime
+hwclock --systohc
+sed -i '177s/.//' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "arch" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+echo root:password | chpasswd
 # INSTALL SCRIPT FOR ARCH PROGRAMS I USE
 # Updating mirrorlist
 #
