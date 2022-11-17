b63f483f990343c7b6046fa3af484708cbe4e51e
abugger1 <abugger1@github.com>
2022-11-16 22:11:05 -0500

    added bash file for easy checkout

---
+++ checkout-bare.cfg.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+sudo rm -rf .local/share/sddm/xorg-session.log
+sudo rm -rf .config/{KDE,gtk-3.0,gtk-4.0,gtkrc,gtkrc-2.0,kde.org,kded5rc,kdedefaults,kdeglobals,kdenlive-layoutsrc,kdenliverc,kitty,neofetch,plasma-localerc,plasma-org.kde.plasma.desktop-appletsrc,plasmarc,plasmashellrc,zsh,zsh~}
+
+sudo rm -rf .gitconfig.gpg
+sudo rm -rf gitignore
+
+sudo rm -rf .icons/Sweet-cursors
+
+sudo rm -rf .kde4/share
+sudo rm -rf .local/share/{color-schemes,konsole,plasma,sddm}
