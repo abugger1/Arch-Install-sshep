acb811faad2b096563856b240057bdc235c61d22
abugger1 <92695670+abugger1@users.noreply.github.com>
2023-09-10 23:30:14 -0400

    updated kde install script

--- arch-base-install-kde.sh
+++ arch-base-install-kde.sh
@@ -10,17 +10,17 @@
 #
  git clone https://aur.archlinux.org/pikaur.git
  cd pikaur
- makepkg -sic --noconfirm
+ makepkg -si 
  cd
  sudo rm -rf pikaur
 #
 #
 #
- sudo pikaur -S powerpill zramd timeshift-bin timeshift-autosnap
+  pikaur -S powerpill zramd timeshift-bin timeshift-autosnap
 #
- sudo systemctl enable --now zramd.service
+  systemctl enable --now zramd.service
 #
- sudo powerpill -S vivid kvantum flameshot python zsh zsh-completions kitty-git kitty-terminfo unzip cmake zoxide fzf
+  powerpill -S vivid kvantum flameshot python zsh zsh-completions kitty-git kitty-terminfo unzip cmake zoxide fzf
 #
 #
 # 
@@ -35,13 +35,13 @@
  sudo pacman -Syy gcc gcc-libs cmake
  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 # install starship
- cargo install nu --features=dataframe
  sudo pacman -S xorg sddm plasma base-devel ruby go nodejs npm fzf ranger lua bitwarden bitwarden-cli firefox-extension-bitwarden libxau libxi libxss libxtst libxcursor libxcomposite libxdamage libxfixes libxrandr libxrender mesa-libgl  alsa-lib libglvnd vlc akonadi-calendar-tools alsa-utils ark artikulate audiocd-kio autoconf automake base bison blinken bluez-utils dolphin-plugins elisa fakeroot filelight flex grantlee-editor gwenview hplip htop inetutils itinerary juk kate kcalc kcharselect kcolorchooser kcron kde-dev-scripts kde-dev-utils kde-gtk-config kdebugsettings kdenetwork-filesharing kdeplasma-addons kdesdk-kio kdesdk-thumbnailers kdf kdialog kfind kfloppy kgamma5 kget kgpg khelpcenter khotkeys kio-gdrive kio-zeroconf kmix kmousetool kontrast ksysguard ksystemlog kteatime ktorrent kwallet-pam kwayland-integration libreoffice-fresh lightdm-webkit-theme-litarvan linux-zen-headers lutris mpv neofetch network-manager-applet okular oxygen palapeli partitionmanager pkgconf plasma-browser-integration plasma-disks plasma-firewall plasma-pa plasma-sdk plasma-systemmonitor plasma-thunderbolt plasma-vault poxml print-manager python-jedi python-pipenv qbittorrent sddm-kcm spectacle stow sweeper flameshot firefox telepathy-kde-approver telepathy-kde-auth-handler telepathy-kde-call-ui telepathy-kde-contact-list telepathy-kde-contact-runner telepathy-kde-desktop-applets telepathy-kde-filetransfer-handler telepathy-kde-send-file telepathy-kde-text-ui neovim konsole
+ cargo install nu --features=dataframe
 #
 # sh -c "$(curl -fsSL https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh)"
  git clone --bare https://github.com/abugger1/.cfg ~/.cfg
  git clone https://github.com/abugger1/neovim ~/.config/nvim
- sudo pacman -S xsel pip npm
+ pikaur -S xsel pip npm
 # pip install pynvim
 # npm -i g neovim
 # npm install plugin-build
@@ -52,5 +52,3 @@
 #
 #
  sudo systemctl enable sddm
- echo "do not forget ranger"
-#
